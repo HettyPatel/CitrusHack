4f60d0f6c1def06b5ec9f9db3e98df3edddd32a3
Het Patel <Hetpatel500@gmail.com>
2023-04-29 17:03:54 -0700

    first commit

---
+++ Realtime.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("/home/hpate061/CitrusHack/runs/detect/train/weights/best.pt")
+
+results = model.predict(source="0", show=True)
+print(results)
