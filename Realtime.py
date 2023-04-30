e5641ae3de1f0bb14a5c3039eb7d70c1e9250f13
Het Patel <Hetpatel500@gmail.com>
2023-04-29 17:43:49 -0700

    added real time script

--- Realtime.py
+++ Realtime.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO("/home/hpate061/CitrusHack/runs/detect/train/weights/best.pt")
+model = YOLO("/Users/hetpatel/CitrusHack/CitrusHack/runs/detect/train/weights/best.pt")
 
 results = model.predict(source="0", show=True)
 print(results)
